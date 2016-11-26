80e9bd47e825dae4f45320dedaf3dc0b072fa3d8
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2016-11-26 17:26:18 +0000

    Updated Fireball to take into account Dragon immunity

--- Assets/Scripts/DecisionMakingActions/Fireball.cs
+++ Assets/Scripts/DecisionMakingActions/Fireball.cs
@@ -8,6 +8,7 @@ namespace Assets.Scripts.DecisionMakingActions
     public class Fireball : WalkToTargetAndExecuteAction
     {
         private int xpChange;
+        private int manaChange;
 
         public Fireball(AutonomousCharacter character, GameObject target) : base("Fireball", character, target)
         {
@@ -21,8 +22,9 @@ namespace Assets.Scripts.DecisionMakingActions
             }
             else if (target.tag.Equals("Dragon"))
             {
-                this.xpChange = 15;
+                this.xpChange = 0;
             }
+            this.manaChange = -5;
         }
 
         public override float GetGoalChange(Goal goal)
@@ -63,9 +65,12 @@ namespace Assets.Scripts.DecisionMakingActions
             var xp = (int)worldModel.GetProperty(Properties.XP);
             worldModel.SetProperty(Properties.XP, xp + this.xpChange);
 
+            var mana = (int)worldModel.GetProperty(Properties.MANA);
+            worldModel.SetProperty(Properties.MANA, mana + this.manaChange);
 
             //disables the target object so that it can't be reused again
-            worldModel.SetProperty(this.Target.name, false);
+            if(!this.Target.tag.Equals("Dragon"))
+                worldModel.SetProperty(this.Target.name, false);
         }
     }
 }
